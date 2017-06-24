b7c52c7bc6620736f87d2e4ce6597c53e604343d
Wasu Winitmontri <khing9277@gmail.com>
2017-06-24 21:26:43 +0700

    create PinCard

---
+++ src/components/Event/PinCard.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const PinCard = (props) => {
+  const {
+    id,
+    name,
+    imageGame,
+    title,
+    createAt,
+    numberUsers,
+    members,
+    userId
+  } = props
+  return (
+    <div className="column is-one-quarter">
+      <Link to={`https://pwa2017-whats-going-on.firebaseio.com/Pin/${id}.json`}>
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-4by3">
+              <img src={imageGame} alt="Image" />
+            </figure>
+          </div>
+          <div className="card-content">
+            <p className="title is-4">{title}</p>
+            <br />
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img src={`http://graph.facebook.com/${userId}/picture?type=normal`} alt="Image" />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-5">{name}</p>
+              </div>
+            </div>
+            <div className="content">
+              <br />
+              <div><i className="fa fa-users middle"></i>{members.length}/{numberUsers}คน</div>
+              <div><i className="fa fa-clock-o middle"></i>{createAt}</div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default PinCard
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+
 import Navbar from './Common/Navbar'
+import PinCard from './Event/PinCard'
 
 class Home extends Component {
+  state = {
+    pins: []
+  }
   componentDidMount() {
-    // axios.get('https://pwa2017-whats-going-on.firebaseio.com/Pin.json', {})
-    // .then(res => {
-    //   console.log(res.data)
-    // })
+    axios.get('https://pwa2017-whats-going-on.firebaseio.com/Pin.json', {})
+    .then(res => {
+      this.setState({
+        pins: res.data
+      }) 
+    })
   }
   render() {
     return (
       <div>
         <Navbar />
-        <div className="columns">
-          <div className="column">
-            First column
-          </div>
-          <div className="column">
-            Second column
-          </div>
-          <div className="column">
-            Third column
-          </div>
-          <div className="column">
-            Fourth column
-          </div>
+        <div className="columns is-multiline is-mobile pin-card">
+          {
+            this.state.pins.map((pin, index) => <PinCard key={`PinCard-${index}`} {...pin} id={index} />)
+          }
         </div>
       </div>
     )
