c97b29495edd47675a5bac62b54f1305e4e0691d
Wasu Winitmontri <khing9277@gmail.com>
2017-06-24 14:42:38 +0700

    restructure file

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react';
+import '../styles/Login.css';
+import FacebookLogin from 'react-facebook-login';
+import bg from '../assets/images/bg.jpeg';
 
-const Login = () => (
-  <div>
-    Login
-  </div>
-)
+class Login extends Component {
 
-export default Login
+  responseFacebook = (response) => {
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <section className="hero is-fullheight" style={{backgroundImage: `url(${bg})`, backgroundSize: 'cover'}}>
+        <div className="hero-body">
+          <div className="container is-center">
+            <div className="wrapper">
+              <FacebookLogin
+                appId="1846600289000828"
+                autoLoad={true}
+                fields="name,email,picture"
+                icon="fa-facebook"
+                textButton="Login Via Facebook"
+                size="metro"
+                callback={this.responseFacebook} />
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Login;
