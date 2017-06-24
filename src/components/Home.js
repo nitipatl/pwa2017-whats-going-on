a667ce1df92e8a7b48da7863918f130046d08be4
Wasu Winitmontri <khing9277@gmail.com>
2017-06-24 20:07:02 +0700

    update auth data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios';
 import Navbar from './Common/Navbar'
 
-const Home = () => (
-  <div>
-    <Navbar />
-    Home
-  </div>
-)
+class Home extends Component {
+  componentDidMount() {
+    // axios.get('https://pwa2017-whats-going-on.firebaseio.com/Pin.json', {})
+    // .then(res => {
+    //   console.log(res.data)
+    // })
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="columns">
+          <div className="column">
+            First column
+          </div>
+          <div className="column">
+            Second column
+          </div>
+          <div className="column">
+            Third column
+          </div>
+          <div className="column">
+            Fourth column
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Home
