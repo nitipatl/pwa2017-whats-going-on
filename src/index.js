811adc9f19fd5cb97683e9065c11212a70695d17
lusiaskolonie <lusiaskolonie@gmail.com>
2017-06-24 12:04:32 +0700

    add google map libs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Map from './Map';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import './styles/bulma.css';
 import './styles/font-awesome.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Map />, document.getElementById('root'));
 registerServiceWorker();
