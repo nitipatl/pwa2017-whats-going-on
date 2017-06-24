d47676073019fb060ae1d79d1e4b0c23e49effa5
Weerapon <top.collection.it@gmail.com>
2017-06-25 00:32:37 +0700

    create call back onchange

--- src/components/Event/New.js
+++ src/components/Event/New.js
@@ -13,6 +13,53 @@ class New extends Component {
     //   storageBucket: "<BUCKET>.appspot.com",
     // };
     // firebase.initializeApp(config);
+    this.state = {
+      title: '',
+      member: '',
+      game: '',
+      numberOfMember: '',
+      description: '',
+    }
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onHandleSelectedMemberChange = this.onHandleSelectedMemberChange.bind(this);
+    this.onHandleSelectedGameChange = this.onHandleSelectedGameChange.bind(this);
+    this.onHandleNumberOfMemberChange = this.onHandleNumberOfMemberChange.bind(this);
+    this.onHandleDescriptionChange = this.onHandleDescriptionChange.bind(this);
+  }
+
+  onTitleChange(event) {
+    this.setState({
+      title: event.target.value,
+    })
+  }
+
+  onHandleSelectedMemberChange(event) {
+    this.setState({
+      member: event.target.value,
+    })
+  }
+
+  onHandleSelectedGameChange(event) {
+    this.setState({
+      game: event.target.value,
+    })
+  }
+
+  onHandleNumberOfMemberChange(event) {
+    this.setState({
+      numberOfMember: event.target.value,
+    })
+  }
+
+  onHandleDescriptionChange(event) {
+    this.setState({
+      description: event.target.value,
+    })
+  }
+
+  onClickSubmit = () => {
+    console.log('submit');
   }
 
   render() {
@@ -25,7 +72,13 @@ class New extends Component {
               <div className="field">
                 <label className="label">Title</label>
                 <p className="control">
-                  <input className="input" type="text" placeholder="Title" />
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="Title"
+                    value={this.state.title}
+                    onChange={this.onTitleChange} 
+                  />
                 </p>
               </div>
             </div>
@@ -34,21 +87,24 @@ class New extends Component {
           <div className="columns field is-horizonta">
             <div className="column is-one-quarters">
               <div className="field">
-                <label className="label">Subject</label>
+                <label className="label">Members</label>
                 <p className="control">
                   <span className="select">
-                    <select>
-                      <option>0</option>
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
-                      <option>6</option>
-                      <option>7</option>
-                      <option>8</option>
-                      <option>9</option>
-                      <option>10</option>
+                    <select
+                      onChange={this.onHandleSelectedMemberChange}
+                      value={this.state.member}
+                    >
+                      <option value="0">0</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
                     </select>
                   </span>
                 </p>
@@ -59,12 +115,15 @@ class New extends Component {
                 <label className="label">Game</label>
                 <p className="control">
                   <span className="select">
-                    <select>
-                      <option>WARCAFT</option>
-                      <option>DOTA2</option>
-                      <option>KAKERLAKEN POKER</option>
-                      <option>catan</option>
-                      <option>uno spin</option>
+                    <select 
+                      onChange={this.onHandleSelectedGameChange}
+                      value={this.state.game}
+                    >
+                      <option value="WARCAFT">WARCAFT</option>
+                      <option value="DOTA2">DOTA2</option>
+                      <option value="KAKERLAKEN POKER">KAKERLAKEN POKER</option>
+                      <option value="catan">catan</option>
+                      <option value="uno spin">uno spin</option>
                     </select>
                   </span>
                 </p>
@@ -74,7 +133,13 @@ class New extends Component {
               <div className="field">
                 <label className="label">Number Of Member</label>
                 <p className="control">
-                  <input className="input" type="text" placeholder="Text input" />
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="Text input"
+                    onChange={this.onHandleNumberOfMemberChange}
+                    value={this.state.numberOfMember}
+                  />
                 </p>
               </div>
             </div>
@@ -85,13 +150,19 @@ class New extends Component {
               <div className="field">
                 <label className="label">Description</label>
                 <p className="control">
-                  <textarea className="textarea" placeholder="Textarea"></textarea>
+                  <textarea
+                    className="textarea"
+                    placeholder="Textarea"
+                    onChange={this.onHandleDescriptionChange}
+                    value={this.state.description}
+                  >
+                  </textarea>
                 </p>
               </div>
             </div>
           </div>
           
-          <a className="button is-primary">Save</a>
+          <a className="button is-primary" onClick={this.onClickSubmit}>Save</a>
         </div>
       </div>
     );
