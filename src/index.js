b64699d0c82cacce017b725d6a8f4cf5c7077313
lusiaskolonie <lusiaskolonie@gmail.com>
2017-06-24 13:31:41 +0700

    add import /app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PinMap from './PinMap';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import './styles/bulma.css';
